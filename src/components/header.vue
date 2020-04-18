e0754ecd2f7a9b3611fc8ad66d70cc45e2ca3309
lvyunze <yunze17817462542@outlook.com>
2020-04-18 19:22:34 +0800

    提交用户端前端基本架子

---
+++ src/components/header.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <el-menu
+      :default-active="activeIndex2"
+      class="el-menu-demo"
+      mode="horizontal"
+      @select="handleSelect"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b"
+    >
+      <el-menu-item index="1">个人课表</el-menu-item>
+      <el-menu-item index="2">校园热门信息</el-menu-item>
+      <el-menu-item index="3">个人设置</el-menu-item>
+      <el-menu-item index="4"></el-menu-item>
+      <el-menu-item index="5">登录</el-menu-item>
+      <el-menu-item index="6">注册</el-menu-item>
+    </el-menu>
+  </div>
+</template>
+<script>
+import Login from "../components/home/Login";
+export default {
+  components: {
+    "v-Login": Login
+  },
+  data() {
+    return {
+      //   activeIndex2: "1"
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
+};
+</script>
+<style lang="">
+.el-menu-demo {
+  padding-left: 60%;
+}
+</style>
---
+++ src/components/home/introduce.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-tabs v-model="activeName" @tab-click="handleClick" class="introduce">
+    <el-tab-pane label="班级管理" name="first">hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh</el-tab-pane>
+    <el-tab-pane label="课表管理" name="second">课表管理</el-tab-pane>
+    <el-tab-pane label="班级资料共享" name="third">资料管理</el-tab-pane>
+    <el-tab-pane label="班费管理" name="fourth">班费管理</el-tab-pane>
+    <el-tab-pane label="班级通讯录" name="five">班级通讯录</el-tab-pane>
+    <el-tab-pane label="班级相册管理" name="six">相册管理</el-tab-pane>
+  </el-tabs>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      activeName: "second"
+    };
+  },
+  methods: {
+    handleClick(tab, event) {
+      console.log(tab, event);
+    }
+  }
+};
+</script>
+<style lang="">
+.introduce {
+  padding-left: 25%;
+  padding-right: 28%;
+}
+</style>
